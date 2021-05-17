409924df955c47e1e03b1ddce58f6da6a76b4157
Ubuntu <ubuntu@ip-172-31-1-114.ap-south-1.compute.internal>
2021-05-17 07:35:36 +0000

    Changes

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Instance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/game/',
+    baseURL: 'http://52.66.249.24/api/game/',
     timeout: 10000,
     headers: {'Content-Type': 'application/json'}
 });
